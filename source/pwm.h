7d8d3d1a40509a4fc314390ef0a253ac4f06d122
Stephan Baerwolf <stephan@matrixstorm.com>
2020-05-01 13:09:57 +0000

    refine selection of currently supported AVRs
    Signed-off-by: Stephan Baerwolf <stephan@matrixstorm.com>

--- source/pwm.h
+++ source/pwm.h
@@ -9,6 +9,18 @@
 
 #include <avr/io.h>
 
+#if (defined (__AVR_ATmega88__)  || defined (__AVR_ATmega88P__)  || defined (__AVR_ATmega88A__)  || defined (__AVR_ATmega88PA__)  || \
+     defined (__AVR_ATmega168__) || defined (__AVR_ATmega168P__) || defined (__AVR_ATmega168A__) || defined (__AVR_ATmega168PA__) || \
+     defined (__AVR_ATmega328__) || defined (__AVR_ATmega328P__) || defined (__AVR_ATmega328A__) || defined (__AVR_ATmega328PA__) || \
+     defined (__AVR_ATmega164__) || defined (__AVR_ATmega164P__) || defined (__AVR_ATmega164A__) || defined (__AVR_ATmega164PA__) || \
+     defined (__AVR_ATmega324__) || defined (__AVR_ATmega324P__) || defined (__AVR_ATmega324A__) || defined (__AVR_ATmega324PA__) || \
+     defined (__AVR_ATmega644__) || defined (__AVR_ATmega644P__) || defined (__AVR_ATmega644A__) || defined (__AVR_ATmega644PA__) || \
+     defined (__AVR_ATmega1284__)|| defined (__AVR_ATmega1284P__)||                                                                  \
+   0)
+#else
+#	error unsupported AVR
+#endif
+
 #ifdef __PWM_C_372746308b4011eaab120800200c9a66
 #	define	PWMPUBLIC
 #else
