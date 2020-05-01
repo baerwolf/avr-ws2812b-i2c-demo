18f8ab08ee49fdba7536237cc9239ef7d1c10fdd
Stephan Baerwolf <stephan@matrixstorm.com>
2020-05-01 11:44:43 +0000

    add one register more to block
    Signed-off-by: Stephan Baerwolf <stephan@matrixstorm.com>

--- defines.h
+++ defines.h
@@ -14,7 +14,7 @@
 
 #ifdef CPUCONTEXTINCLUDEDEFINES
 #   include <stdint.h>
- /* our WS2812B timing driver needs registers r2 to r6 exclusively for its own */
+ /* our WS2812B timing driver needs registers r2 to r7 exclusively for its own */
  
  /* tell the compiler to block the registers and avoid generating code with them */
  register uint8_t __register2 asm ("r2");
@@ -22,13 +22,15 @@
  register uint8_t __register4 asm ("r4");
  register uint8_t __register5 asm ("r5");
  register uint8_t __register6 asm ("r6");
-
+ register uint8_t __register6 asm ("r7");
+ 
  /* inform cpucontext library NOT to switch it between contexts */
 #   define CONFIG_CPUCONTEXT_NO_R2
 #   define CONFIG_CPUCONTEXT_NO_R3
 #   define CONFIG_CPUCONTEXT_NO_R4
 #   define CONFIG_CPUCONTEXT_NO_R5
 #   define CONFIG_CPUCONTEXT_NO_R6
+#   define CONFIG_CPUCONTEXT_NO_R7
 #endif
 
 #endif
